97adf305df27c5fb52f354b25bbbabf753641a1d
Carina Chenot <mllecarinach@gmail.com>
2017-11-15 16:55:33 +0100

    Add responsive section about

--- wp-content/themes/ghibli/home.php
+++ wp-content/themes/ghibli/home.php
@@ -28,16 +28,18 @@ $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
   <div class="about-image">
     <img src="<?= THEME_URL . '/dist/img/miyasaki.jpeg' ?>">
   </div>
-  <div class="about-text">
-    <h3 class="about-title">Découvrez toute l’histoire du studio et de ses grands fondateurs.</h3>
-    <p class="about-paragraph">
-      Le studio Ghibli est un studio d'animation japonais, il a été officiellement fondé, par Hayao Miyazaki et Isao Takahata. Il produit des films d'animation japonais, longs-métrages et courts-métrages, ainsi que des téléfilms, des séries d'animation et des jeux vidéo, dans une moindre mesure.
-    </p>
+  <div class="about-content">
+    <div class="about-text">
+      <h3 class="about-title">Découvrez toute l’histoire du studio et de ses grands fondateurs.</h3>
+      <p class="about-paragraph">
+        Le studio Ghibli est un studio d'animation japonais, il a été officiellement fondé, par Hayao Miyazaki et Isao Takahata. Il produit des films d'animation japonais, longs-métrages et courts-métrages, ainsi que des téléfilms, des séries d'animation et des jeux vidéo, dans une moindre mesure.
+      </p>
+    </div>
+    <a href="#" class="btn-scale about-cta">
+      <span class="about-cta-text">À propos de nous</span>
+      <?php include THEME_PATH . '/dist/img/right-arrow.svg' ?>
+    </a>
   </div>
-  <a href="#" class="btn-scale about-cta">
-    <span class="about-cta-text">À propos de nous</span>
-    <?php include THEME_PATH . '/dist/img/right-arrow.svg' ?>
-  </a>
 </section>
 
 <?php
