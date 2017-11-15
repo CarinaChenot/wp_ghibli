cefcbb80ac92661d9398ace0cf837743cdc0bcc2
Carina Chenot <mllecarinach@gmail.com>
2017-11-15 12:28:43 +0100

    Add footer (#1)

--- wp-content/themes/ghibli/footer.php
+++ wp-content/themes/ghibli/footer.php
@@ -1,5 +1,24 @@
-<footer id="footer">
-
+<footer class="footer">
+  <div class="footer-main">
+    <div class="logo">
+      <img src="<?= THEME_URL . '/dist/img/totoro.jpg' ?>">
+    </div>
+    <?php
+    $menuArgs = array(
+      'theme_location' => 'main_menu',
+      'container'       => 'nav',
+      'container_class' => 'main-menu',
+      'items_wrap' => '%3$s',
+    );
+    wp_nav_menu( $menuArgs ); ?>
+    <ul class="social-list">
+      <li class="social-item"><?php include THEME_PATH . '/dist/img/facebook.svg'; ?></li>
+      <li class="social-item"><?php include THEME_PATH . '/dist/img/twitter.svg'; ?></li>
+    </ul>
+  </div>
+  <div class="footer-legal">
+  © Studio Ghibli, all rights reserved
+  </div>
 </footer>
 
 <?php wp_footer();  ?>
