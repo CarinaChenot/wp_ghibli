a8717aa784c4af2e604861e209f7f74e6cb1ab0e
Carina Chenot <mllecarinach@gmail.com>
2017-11-17 14:31:26 +0100

    Fix footer menu

--- web/app/themes/index/inc/menu.php
+++ web/app/themes/index/inc/menu.php
@@ -1,19 +1,17 @@
 <?php
 
 // Main menu //
-
 function register_menus() {
   register_nav_menus(array(
-      'theme_location' => 'main_menu',
-      'container'       => 'nav',
-      'container_class' => 'main-menu',
-      'items_wrap' => '%3$s',
+	'theme_location' => 'main_menu',
   ));
 }
+
 add_action( 'init', 'register_menus' );
 
-add_filter('timber/context', 'init_timber_menu');
-function init_timber_menu($context){
-    $context['mainMenu'] = new TimberMenu('mainMenu');
-    return $context;
+add_filter( 'timber/context', 'init_timber_menu' );
+
+function init_timber_menu($context) {
+	$context['mainMenu'] = new TimberMenu( 'mainMenu' );
+	return $context;
 }
