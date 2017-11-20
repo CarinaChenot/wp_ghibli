98de5baef5409040e1506ee36b3a22ebef56398b
Carina Chenot <mllecarinach@gmail.com>
2017-11-21 00:33:19 +0100

    Fix header

--- web/app/themes/index/index.php
+++ web/app/themes/index/index.php
@@ -5,6 +5,7 @@ Template Name: Page Home
 
 $context = Timber::get_context();
 
+$context['post'] = new Timber\Post();
 $context['famous_movies'] = wpPostsToTimber(get_field('famous_movies'));
 $context['latest_movies'] = getRecentMovies(get_field('recent_movies_number'));
 
