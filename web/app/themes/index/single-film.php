085e27adb0135dfa08ef8f8f78f1d4d258d938fd
Carina Chenot <mllecarinach@gmail.com>
2017-11-21 15:58:00 +0100

    clean single films

--- web/app/themes/index/single-film.php
+++ web/app/themes/index/single-film.php
@@ -1,7 +1,8 @@
 <?php
 
 $context = Timber::get_context();
+
 $context['post'] = new Timber\Post();
 $context['acf'] = get_field_objects($context['post']->ID);
-dump($context);
+
 Timber::render('views/pages/single-film.twig', $context);
--- web/app/themes/index/single.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$timber = new \Timber\Timber();
-$context = Timber::get_context();
-
-//dump($context);
-Timber::render('views/pages/movie.twig', $context);
