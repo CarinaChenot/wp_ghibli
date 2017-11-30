76222876ce636fcb0ea8f2d1cd4da38eeed436e0
Carina Chenot <mllecarinach@gmail.com>
2017-11-30 20:02:08 +0100

    Add anims + ignore dist

--- web/app/themes/index/page-templates/page-about.php
+++ web/app/themes/index/page-templates/page-about.php
@@ -7,5 +7,13 @@ $context = Timber::get_context();
 
 $context['post'] = new Timber\Post();
 
-//dump($context);
+$args = array(
+  'post_type' => 'author',
+  'posts_per_page' => -1,
+);
+
+$context['authors'] = Timber::get_posts( $args );
+
+dump($context);
+
 Timber::render('views/pages/about.twig', $context);
