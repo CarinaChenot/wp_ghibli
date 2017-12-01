f0b5784b3185124e77c9037cf56bf7e8f3e071cd
Carina Chenot <mllecarinach@gmail.com>
2017-12-01 13:54:43 +0100

    add founders

--- web/app/themes/index/page-templates/page-about.php
+++ web/app/themes/index/page-templates/page-about.php
@@ -10,7 +10,6 @@ $context['sections'][] = new Timber\Post($context['post']->custom['section_1']);
 $context['sections'][] = new Timber\Post($context['post']->custom['section_2']);
 $context['sections'][] = new Timber\Post($context['post']->custom['section_3']);
 $context['sections'][] = new Timber\Post($context['post']->custom['section_4']);
-//$context['sections'][] = new Timber\Post($context['post']->custom['section_1']);
 
 $args = array(
   'post_type' => 'author',
@@ -19,5 +18,4 @@ $args = array(
 
 $context['authors'] = Timber::get_posts( $args );
 
-//dump($context);
 Timber::render('views/pages/about.twig', $context);
