98b268c33c29d19724989278aad6f57d5c67170e
Paul-maire <paul.maire@hetic.net>
2017-12-01 01:06:27 +0100

    template twig about

--- web/app/themes/index/page-templates/page-about.php
+++ web/app/themes/index/page-templates/page-about.php
@@ -6,6 +6,9 @@ Template Name: Page About
 $context = Timber::get_context();
 
 $context['post'] = new Timber\Post();
+$context['sections'][] = new Timber\Post($context['post']->custom['section_1']);
+$context['sections'][] = new Timber\Post($context['post']->custom['section_2']);
+//$context['sections'][] = new Timber\Post($context['post']->custom['section_1']);
 
 //dump($context);
 Timber::render('views/pages/about.twig', $context);
