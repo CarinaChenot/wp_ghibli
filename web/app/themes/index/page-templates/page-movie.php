6c4d3b0474d1b52a5c7a3ae8a2bfa799a7e7ce3f
Paul-maire <paul.maire@hetic.net>
2017-12-01 02:37:29 +0100

    template twig films

--- web/app/themes/index/page-templates/page-movie.php
+++ web/app/themes/index/page-templates/page-movie.php
@@ -3,6 +3,16 @@
 Template Name: Page Movie
 */
 
+
 $context = Timber::get_context();
 
+$args = array(
+'post_type' => 'film',
+    'posts_per_page' => -1,
+);
+
+$context['films'] = Timber::get_posts( $args );
+$context['post'] = new \Timber\Post();
+
+//dump($context);
 Timber::render('views/pages/archive-film.twig', $context);
