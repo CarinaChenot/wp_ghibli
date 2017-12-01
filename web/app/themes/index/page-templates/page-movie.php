57d93feef3e760b83d56584b6ef20a059d89a9d6
Carina Chenot <mllecarinach@gmail.com>
2017-12-01 12:42:07 +0100

    Add pagination

--- web/app/themes/index/page-templates/page-movie.php
+++ web/app/themes/index/page-templates/page-movie.php
@@ -3,16 +3,20 @@
 Template Name: Page Movie
 */
 
+if (!isset($paged) || !$paged){
+    $paged = 1;
+}
 
 $context = Timber::get_context();
 
 $args = array(
-'post_type' => 'film',
-    'posts_per_page' => -1,
+    'post_type' => 'film',
+    'posts_per_page' => 6,
+    'paged' => $paged
 );
 
-$context['films'] = Timber::get_posts( $args );
 $context['post'] = new \Timber\Post();
+$context['films'] = new \Timber\PostQuery($args);
 
-//dump($context);
+dump($context);
 Timber::render('views/pages/archive-film.twig', $context);
